f739d8f1ee5659f78feb25f34033bb7c563c66a0
Christoffer Vølund <cvolund@Christoffers-MacBook-Pro.local>
2016-10-12 15:15:41 +0200

    Merged

--- app/app/controllers/index.js
+++ app/app/controllers/index.js
@@ -2,14 +2,28 @@ var controllers = {
 	home: Alloy.createController('home'),
 	map: Alloy.createController('map'),
 	groups: Alloy.createController('groups'),
-	schedule: Alloy.createController('schedule')
+	schedule: Alloy.createController('schedule'),
+	more: Alloy.createController("more")
 };
 var views = {
-	
+	_setActiveMenu: function(v){
+		//set all menues to inactive
+		$.nav.children.forEach(function(tab){
+			tab.children.forEach(function(element){
+				$.removeClass($[element.id], "active");
+			});
+		});
+		
+		//set the selected menu to active
+		$["tab_"+v].children.forEach(function(element){
+			$.addClass($[element.id], "active");
+		});
+	},
+
 	_show: function(v){
 		$.view.removeAllChildren();//remove all content from view
 		$.view.add([ controllers[v].getView() ]);//add new content to the view
-		//views._setActiveMenu("home");
+		views._setActiveMenu(v); //highligth selected menu item at the bottom
 	},
 	map: function(){
 		views._show("map");
@@ -23,11 +37,12 @@ var views = {
 	schedule: function(){
 		views._show("schedule");
 	},
-	
 	more: function(){
-		//laves på en anden måde end de andre views, da det er et overlay.
+		views._show("more");	
 	}
 };
 
+
 $.index.addEventListener("open", views.groups); //make the default view = home
+
 $.index.open();
