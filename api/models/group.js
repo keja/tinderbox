13bf1136a7443b1907e3b11962d51fc3eafc6d45
Kenneth Jakobsen <keja@users.noreply.github.com>
2017-04-24 21:35:13 +0200

    Delete group.js

--- api/models/group.js
+++
@@ -1,8 +0,0 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-module.exports = mongoose.model('Group', new Schema({
-    name: String,
-    members: Array,
-    artists: Array
-}));
